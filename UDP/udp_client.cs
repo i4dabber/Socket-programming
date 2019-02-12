a20569b4bc6e4f1376b7b036924d31c4cbba30d6
i4dabber <43787841+i4dabber@users.noreply.github.com>
2019-02-12 12:44:38 +0100

    Add files via upload

---
+++ UDP/udp_client.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace udp
+{
+	class UDPClient
+	{
+		const int PORT = 9000;
+
+		private UDPClient(string[] args)
+		{
+			// Validation of parameter list
+			if(args.Length != 2) 
+			{
+				Console.WriteLine("Incorrect parameterlist.");
+				Console.WriteLine("Please type-in valid hostname and valid input for reading uptime and loadavg");
+				Environment.Exit(1);
+			}
+
+			// Creation of new UDPClient
+			var client = new UdpClient();
+
+			// Connecting to end point on host with the PORT 9000
+			IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse(args[0]), PORT);
+			client.Connect(endPoint);
+
+			// Read request sent to server
+			byte[] DataSent = Encoding.ASCII.GetBytes(args[1]);
+			client.Send(DataSent, DataSent.Length);
+
+			// Output received data from server on screen
+			byte[] DataReceived = client.Receive(ref endPoint);
+			string data = Encoding.ASCII.GetString(DataReceived);
+			Console.WriteLine(data);
+		}
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Client starts...");
+			new UDPClient(args);
+		}
+	}
+}
+
---
+++ UDP/udp_server.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace udp
+{
+	class UDPServer
+	{
+		const int PORT = 9000;
+
+		private UDPServer()
+		{
+			var server = new UdpClient(PORT);
+
+			// Endpoint establishment
+			IPEndPoint endPoint = new IPEndPoint(IPAddress.Any, PORT);
+
+			while (true) 
+			{
+				// Input from client
+				byte[] DataReceived = server.Receive(ref endPoint); //listen on port 9000
+				string data = Encoding.ASCII.GetString(DataReceived);
+
+				Console.WriteLine ("Client input received: " + data);
+
+				// Get info
+				string measurement = GetMeasurement(data);
+
+				// Send data back
+				byte[] DataSent = Encoding.ASCII.GetBytes(measurement);
+				server.Send(DataSent, DataSent.Length, endPoint);					
+			}
+		}
+
+		public string GetMeasurement(string letter)
+		{
+			string fileLocation;
+
+			switch(letter = letter.ToUpper()) 
+			{
+				case "U":
+					fileLocation = "/proc/uptime";
+					Console.WriteLine("Now sending uptime...");
+					break;
+				case "L":
+					fileLocation = "/proc/loadavg";
+					Console.WriteLine("Now sending loadavg...");
+					break;
+				default:
+					Console.WriteLine("Incorrect input. Now sending error message...");
+					return "Incorrect input. Acceptable inputs are l (L) or u (U)\n";
+			}
+			return "Reading from " + fileLocation + ": " + File.ReadAllText(fileLocation);
+		}
+	
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Server starts...");
+			new UDPServer();
+		}
+	}
+}
