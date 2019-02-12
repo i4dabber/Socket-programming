27492cbb3fa67fca780a74afdc1c238b54eff718
i4dabber <43787841+i4dabber@users.noreply.github.com>
2019-02-12 12:43:51 +0100

    Add files via upload

---
+++ TCP/clientside/clientside/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace tcp
+{
+    class file_client
+    {
+        /// <summary>
+        /// The PORT.
+        /// </summary>
+        const int PORT = 9000;
+        /// <summary>
+        /// The BUFSIZE.
+        /// </summary>
+        const int BUFSIZE = 1000;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="file_client"/> class.
+        /// </summary>
+        /// <param name='args'>
+        /// The command-line arguments. First ip-adress of the server. Second the filename
+        /// </param>
+        private file_client(string[] args)
+        {
+            TcpClient client = new TcpClient();
+            
+            //Sikrer connection til det angivne Port
+            client.Connect(args[0], PORT);
+            Console.WriteLine($"'{args[0]}' Has been connected <---");
+
+			//Requester client streamet
+            NetworkStream server = client.GetStream(); 
+            Console.WriteLine($"'{args[1]}'Filename requested <---");
+
+            string File_Name = args[1];
+            LIB.writeTextTCP(server, File_Name);
+
+            long File_Size = LIB.getFileSizeTCP(server);
+
+            //Sædvanlige check om filen eksistere eller ej
+            while (File_Size == 0)
+            {
+                Console.WriteLine("File not found!");
+				File_Name = Console.ReadLine();
+
+                Console.WriteLine($"Requesting this file ---> '{File_Name}'");
+
+                LIB.writeTextTCP(server, File_Name);
+                File_Size = LIB.getFileSizeTCP(server);
+            }
+
+            Console.WriteLine("Size of file ---> " + File_Size);
+
+            receiveFile(File_Name, server, File_Size);
+        }
+
+        
+        /// <param name='fname'>
+        /// File name.
+        /// </param>
+        /// <param name='output'>
+        /// Network stream for reading from the server
+        /// </param>
+        private void receiveFile(String fname, NetworkStream output, long file_size)
+        {
+            fname = LIB.extractFileName(fname);
+
+            //Opretter mappe og putter ind til denne directory
+            string data_directory = "/root/Desktop/TilTorben/";
+            Directory.CreateDirectory(data_directory);
+            FileStream file = new FileStream(data_directory + fname, FileMode.Create, FileAccess.Write);
+            
+			byte[] data = new byte[BUFSIZE];
+
+            
+            int bytes_total = 0;
+            int bytes_read;
+
+            Console.WriteLine("Reading file! " + fname);
+
+           //Løkke - der tjekker størrelsen af filen og skriver til bytes read - som læses fra i cw
+            while (file_size > bytes_total)
+            {
+                bytes_read = output.Read(data, 0, data.Length);
+                file.Write(data, 0, bytes_read);
+
+                bytes_total += bytes_read;
+
+                Console.WriteLine("Reading bytes = " + bytes_read.ToString() + "\t Total bytes read = " + bytes_total);
+
+            }
+
+            Console.WriteLine("File received");
+        }
+
+        /// <summary>
+        /// The entry point of the program, where the program control starts and ends.
+        /// </summary>
+        /// <param name='args'>
+        /// The command-line arguments.
+        /// </param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Client...");
+            new file_client(args);
+        }
+    }
+}
---
+++ TCP/serverside/serverside/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+
+namespace tcp
+{
+    class file_server
+    {
+        /// <summary>
+        /// The PORT
+        /// </summary>
+        const int PORT = 9000;
+        // const string = "HANS";
+        /// <summary>
+        /// The BUFSIZE
+        /// </summary>
+        const int BUFSIZE = 1000;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="file_server"/> class.
+        /// </summary>
+        private file_server()
+        {
+            // Starts server and accepts a client
+            Console.WriteLine("Starting server - waiting for client --->");
+
+            //Opretter en socket
+            TcpListener Server_Socket = new TcpListener(IPAddress.Any, PORT);
+            TcpClient Client_Socket = default(TcpClient);
+
+            //Starter serveren og connectecter til client vha Accept funktionen
+            Server_Socket.Start();
+            Client_Socket = Server_Socket.AcceptTcpClient();
+
+            Console.WriteLine("Connected to client!");
+
+            // getter client stream
+            NetworkStream client = Client_Socket.GetStream();
+
+            //Istantiering af filename til at læse og checke om file size eksistere
+            string File_Name = LIB.readTextTCP(client);
+            long File_Size = LIB.check_File_Exists(File_Name);
+
+            // loop - der tjekker om filen er fundet.
+            while (File_Size == 0)
+            {
+                string msg = "File '" + File_Name + "' not found";
+                Console.WriteLine(msg);
+
+                LIB.writeTextTCP(client, File_Size.ToString());
+
+                File_Name = LIB.readTextTCP(client);
+                File_Size = LIB.check_File_Exists(File_Name);
+
+            }
+
+            //Udskriver filnavn og størrelsen som er initialiseret
+            Console.WriteLine("Filename: " + File_Name);
+            Console.WriteLine("Size: " + File_Size);
+            //Skriver texten til client stream
+            LIB.writeTextTCP(client, File_Size.ToString());
+
+            sendFile(File_Name, client);
+
+            //Lukker programmet
+            Client_Socket.Close();
+            Server_Socket.Stop();
+        }
+
+       
+        /// <param name='fName'>
+        /// </param>
+        /// <param name='fSize'> 
+        /// </param>
+        /// <param name='output'>
+        /// </param>
+        private void sendFile(String fName, NetworkStream output)
+        {
+            Console.WriteLine("Sending the requested file --->");
+
+			FileStream fStream = new FileStream(fName, FileMode.Open, FileAccess.Read);
+            Byte[] fSend = new Byte[BUFSIZE]; //bytes som 
+
+            int bytes = 0;
+
+            //Bliver ved med at sende indtil intet bytes er tilbage
+            while ((bytes = fStream.Read(fSend, 0, fSend.Length)) > 0) 
+            {
+                //outputter clientstream byted som er sendt
+				output.Write(fSend, 0, bytes); 
+
+                Console.WriteLine($"Send {bytes} bytes");
+
+            }
+            Console.WriteLine("File has been sent <---");
+        }
+
+
+        /// <param name='args'>
+        /// </param>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Server starts...");
+            //løkken der kører vores funktioner
+            while (true)
+            {
+                new file_server();
+            }
+        }
+
+    }
+}
+
+
